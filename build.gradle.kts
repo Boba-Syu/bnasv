eb3d020ffbb5fca6ba4400c693d4643764a8684e
Boba-syu <bobasyu@163.com>
2024-07-24 14:28:24 +0800

    配置修改

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,32 +4,35 @@ plugins {
 
 group = "cn.bobasyu"
 version = "1.0-SNAPSHOT"
-
 repositories {
     maven { setUrl("https://maven.aliyun.com/nexus/content/groups/public") }
     mavenCentral()
 }
 
+val vertxVersion = "4.4.0"
+val jacksonVersion = "2.17.2"
+val slf4jVersion = "1.5.6"
+
 dependencies {
     testImplementation(kotlin("test"))
 
-    implementation("io.vertx:vertx-core:4.4.0")
-    implementation("io.vertx:vertx-lang-kotlin:4.4.0")
-    implementation("io.vertx:vertx-web:4.4.0")
-    implementation("io.vertx:vertx-auth-jwt:4.4.0")
-    implementation("io.vertx:vertx-auth-common:4.4.0")
-    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.4.0")
-    implementation("io.vertx:vertx-mysql-client:4.4.0")
+    implementation("io.vertx:vertx-core:${vertxVersion}")
+    implementation("io.vertx:vertx-lang-kotlin:${vertxVersion}")
+    implementation("io.vertx:vertx-web:${vertxVersion}")
+    implementation("io.vertx:vertx-auth-jwt:${vertxVersion}")
+    implementation("io.vertx:vertx-auth-common:${vertxVersion}")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:${vertxVersion}")
+    implementation("io.vertx:vertx-mysql-client:${vertxVersion}")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
 
-    implementation("org.slf4j:slf4j-jdk14:1.5.6")
+    implementation("org.slf4j:slf4j-jdk14:${slf4jVersion}")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(11)
 }
