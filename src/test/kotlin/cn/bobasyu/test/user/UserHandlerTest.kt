0414c49825ce59e72ac6cac0a1a8837d398c19c3
Boba-syu <bobasyu@163.com>
2024-08-06 13:47:11 +0800

    登录接口单元测试

--- src/test/kotlin/cn/bobasyu/test/user/UserHandlerTest.kt
+++ src/test/kotlin/cn/bobasyu/test/user/UserHandlerTest.kt
@@ -5,16 +5,16 @@ import cn.bobasyu.MainVerticle
 import cn.bobasyu.base.ApplicationContext
 import cn.bobasyu.base.HttpResult
 import cn.bobasyu.user.AbstractUserRepository
+import cn.bobasyu.user.UserLoginDTO
 import cn.bobasyu.user.UserRecord
 import cn.bobasyu.user.UserVerticle
 import cn.bobasyu.utils.parseJson
 import cn.bobasyu.utils.toJson
 import io.vertx.core.Future
 import io.vertx.core.Vertx
-import io.vertx.ext.auth.User
+import io.vertx.core.buffer.Buffer
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.client.WebClient
-import io.vertx.ext.web.codec.BodyCodec
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import io.vertx.kotlin.coroutines.await
@@ -68,16 +68,31 @@ class UserHandlerTest {
             val userRecord = UserRecord(1, "test1", "testPassword1", LocalDateTime.now(), LocalDateTime.now())
             Mockito.`when`(userRepositoryVerticle.queryUserById(1)).thenReturn(Future.succeededFuture(userRecord))
 
-            val client = WebClient.create(Vertx.vertx())
-            val httpResponse = client.get("/user/query/id/1")
-//                .addQueryParam("id", "1")
-                .send()
-                .await()
+            val client = WebClient.create(vertx)
+            val httpResponse = client.get("/user").addQueryParam("id", "1").send().await()
 
             val resp: HttpResult<*> = httpResponse.body().toString().parseJson(HttpResult::class.java)
             val data = resp.data!!.toJson().parseJson(UserRecord::class.java)
+
             assertEquals(data, userRecord)
             Mockito.verify(userRepositoryVerticle, Mockito.times(1)).queryUserById(1)
+        }
+        testContext.completeNow()
+    }
+
+    @Test
+    fun loginTest(vertx: Vertx, testContext: VertxTestContext) {
+        runBlocking {
+            val userRecord = UserRecord(1, "test1", "testPassword1", LocalDateTime.now(), LocalDateTime.now())
+            val userLoginDTO = UserLoginDTO("test1", "testPassword1")
+            Mockito.`when`(userRepositoryVerticle.queryUserByUsernameAndPassword(userLoginDTO))
+                .thenReturn(Future.succeededFuture(userRecord))
+
+            val client = WebClient.create(vertx)
+            val httpResponse = client.post("/login").sendBuffer(Buffer.buffer(userLoginDTO.toJson())).await()
+
+            println(httpResponse.body())
+            Mockito.verify(userRepositoryVerticle, Mockito.times(1)).queryUserByUsernameAndPassword(userLoginDTO)
         }
         testContext.completeNow()
     }
