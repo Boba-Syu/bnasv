be4cc3f969adea447f4fac104df234f942fb97cc
Boba-syu <bobasyu@163.com>
2024-11-04 15:57:11 +0800

    尝试hibernate

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ val vertxVersion = "4.4.0"
 val jacksonVersion = "2.17.2"
 val slf4jVersion = "1.5.6"
 val mockitoVersion = "5.12.0"
+val hibernateVersion = "5.6.15.Final"
+val postgresqlVersion = "42.7.4"
 
 dependencies {
     testImplementation(kotlin("test"))
@@ -27,6 +29,9 @@ dependencies {
     implementation("io.vertx:vertx-auth-common:${vertxVersion}")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:${vertxVersion}")
     implementation("io.vertx:vertx-mysql-client:${vertxVersion}")
+
+    implementation("org.hibernate:hibernate-entitymanager:${hibernateVersion}")
+    implementation("org.postgresql:postgresql:${postgresqlVersion}")
 
     implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
--- src/main/kotlin/cn/bobasyu/user/UserRepository.kt
+++ src/main/kotlin/cn/bobasyu/user/UserRepository.kt
@@ -8,9 +8,14 @@ import cn.bobasyu.databeses.SqlGenerator
 import cn.bobasyu.entity.UserInsertDTO
 import cn.bobasyu.entity.UserLoginDTO
 import cn.bobasyu.entity.UserRecord
+import cn.bobasyu.utils.camelToSnakeCase
+import cn.bobasyu.utils.toJson
 import io.vertx.core.Future
 import io.vertx.core.eventbus.Message
 import io.vertx.kotlin.coroutines.await
+import sun.jvm.hotspot.HelloWorld.e
+import javax.persistence.EntityManager
+import javax.persistence.Persistence
 
 
 /**
@@ -21,21 +26,33 @@ open class UserRepositoryVerticle(
 ) : AbstractUserRepository(applicationContext) {
     private val mySqlClient: MySqlClient = applicationContext.mySqlClient
 
+    val entityManager: EntityManager by lazy {
+        val factory = Persistence.createEntityManagerFactory("bnasv-hibernate")
+        factory.createEntityManager()
+    }
+
     override suspend fun start() {
         super.start()
     }
 
     override suspend fun handleQueryUserListEvent(message: Message<Unit>) {
-        queryUserList()
-            .onSuccess { userList: List<UserRecord> -> message.reply(userList) }
-            .onFailure { message.fail(500, it.message) }
+        val userId = message.body()
+        try {
+            val userRecordList = queryUserList()
+            message.reply(userRecordList)
+        } catch (e: Exception) {
+            message.fail(500, e.message)
+        }
     }
 
     override suspend fun handleQueryUserByIdEvent(message: Message<Int>) {
         val userId = message.body()
-        queryUserById(userId)
-            .onSuccess { message.reply(it as UserRecord) }
-            .onFailure { message.fail(500, it.message) }
+        try {
+            val userRecord = queryUserById(userId)
+            message.reply(userRecord)
+        } catch (e: Exception) {
+            message.fail(500, e.message)
+        }
     }
 
     override suspend fun handleInsertUserEvent(message: Message<UserInsertDTO>) {
@@ -52,9 +69,8 @@ open class UserRepositoryVerticle(
 
     override suspend fun handleQueryUserByUsernameAndPasswordEvent(message: Message<UserLoginDTO>) {
         val userLoginDTO: UserLoginDTO = message.body()
-        queryUserByUsernameAndPassword(userLoginDTO)
-            .onSuccess { userList -> message.reply(userList) }
-            .onFailure { message.fail(500, it.message) }
+        val userList = queryUserByUsernameAndPassword(userLoginDTO)
+        message.reply(userList)
     }
 
     private fun queryUsernameExist(username: String): Future<Boolean> {
@@ -65,37 +81,29 @@ open class UserRepositoryVerticle(
             .map { return@map (it as List<*>).isNotEmpty() }
     }
 
-    private fun queryUserList(): Future<List<UserRecord>> {
-        val queryListSql: String = SqlGenerator(UserRecord::class).select().generate()
-        return mySqlClient.query(queryListSql, UserRecord::class.java)
+    private fun queryUserList(): List<UserRecord> {
+        val tableName = UserRecord::class.simpleName!!.camelToSnakeCase()
+        return entityManager.createQuery(tableName, UserRecord::class.java).resultList
     }
 
-    private fun queryUserById(id: Int): Future<UserRecord> {
-        return SqlGenerator(UserRecord::class)
-            .select()
-            .where().eq(UserRecord::userId, id)
-            .execute(mySqlClient)
-            .map {
-                if ((it as List<*>).isEmpty()) {
-                    throw NoSuchRecordInDatabaseException("id: $id")
-                }
-                it.first()
-            }
-            .map { it as UserRecord }
+    private fun queryUserById(id: Int): UserRecord {
+        val userRecord: UserRecord? = entityManager.find(UserRecord::class.java, id)
+        if (userRecord == null) {
+            throw NoSuchRecordInDatabaseException("id: $id")
+        }
+        return userRecord
+
     }
 
-    private fun queryUserByUsername(username: String): Future<UserRecord> {
-        return SqlGenerator(UserRecord::class)
-            .select()
-            .where().eq(UserRecord::username, username)
-            .execute(mySqlClient)
-            .map {
-                if ((it as List<*>).isEmpty()) {
-                    throw NoSuchRecordInDatabaseException("username: $username")
-                }
-                it.first()
-            }
-            .map { it as UserRecord }
+    private fun queryUserByUsername(username: String): UserRecord {
+        val sql = "SELECT * FROM ${UserRecord::class.simpleName!!.camelToSnakeCase()} WHERE username = :username"
+        val query = entityManager.createQuery(sql, UserRecord::class.java)
+        query.setParameter("username", username)
+        val userRecordList: List<UserRecord> = query.resultList
+        if (userRecordList.isEmpty()) {
+            throw NoSuchRecordInDatabaseException("username: $username")
+        }
+        return userRecordList.first()
     }
 
     private fun insertUser(userInsertDTO: UserInsertDTO): Future<Unit> {
@@ -106,16 +114,15 @@ open class UserRepositoryVerticle(
             .map {}
     }
 
-    private fun queryUserByUsernameAndPassword(userLoginDTO: UserLoginDTO): Future<UserRecord> {
-        return SqlGenerator(UserRecord::class).select()
-            .where().eq(UserRecord::username, userLoginDTO.username)
-            .and().eq(UserRecord::password, userLoginDTO.password)
-            .execute(mySqlClient)
-            .map {
-                if ((it as List<*>).isEmpty()) {
-                    throw BaseException(message = "username or password error")
-                }
-                it.first()
-            }.map { it as UserRecord }
+    private fun queryUserByUsernameAndPassword(userLoginDTO: UserLoginDTO): UserRecord {
+        val sql = "SELECT * FROM ${UserRecord::class.simpleName!!.camelToSnakeCase()} " +
+                "WHERE username = :username"
+        val query = entityManager.createQuery(sql, UserRecord::class.java)
+        query.setParameter("username", userLoginDTO.username)
+        val userRecordList: List<UserRecord> = query.resultList
+        if (userRecordList.isEmpty()) {
+            throw NoSuchRecordInDatabaseException("UserLoginDTO: ${userLoginDTO.toJson()}")
+        }
+        return userRecordList.first()
     }
 }
