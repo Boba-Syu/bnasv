641f51607544aa8a9542a7cb13c3f6bae936bae2
Boba-syu <bobasyu@163.com>
2024-12-05 14:34:18 +0800

    json序列化测试

--- src/main/kotlin/cn/bobasyu/entity/Bangumi.kt
+++ src/main/kotlin/cn/bobasyu/entity/Bangumi.kt
@@ -1,11 +1,16 @@
 package cn.bobasyu.entity
 
-import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 import io.vertx.core.json.JsonObject
 import java.time.LocalDate
 
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 enum class BangumiSubjectType(
+    @JsonValue
     val code: Int,
+
     val description: String,
 ) {
     BOOK(1, "书籍"),
@@ -17,9 +22,13 @@ enum class BangumiSubjectType(
 
 data class BangumiImages(
     val large: String = "",
+
     val common: String = "",
+
     val medium: String = "",
+
     val small: String = "",
+
     val grid: String = "",
 )
 
@@ -30,33 +39,33 @@ data class BangumiSubject(
 
     val name: String = "",
 
-    @JsonAlias("name_cn")
+    @JsonProperty("name_cn")
     val nameCn: String = "",
 
-    val summary: String,
+    val summary: String = "",
 
     val date: LocalDate,
 
-    val platform: String,
+    val platform: String = "",
 
     val images: BangumiImages = BangumiImages(),
 
-    val infoBox: JsonObject = JsonObject(),
+    val infoBox: String = "",
 
     /**
      * 书籍条目的册数
      */
     val volumes: Int? = null,
+
     /**
      * 数据库中的章节数量
-     *
-     *
      */
-    @JsonAlias("total_episodes")
+    @JsonProperty("total_episodes")
     val totalEpisodes: Int? = null,
 )
 
 data class BangumiSearchDto(
     val keyword: String,
+
     val types: List<BangumiSubjectType>? = null
 )
--- src/test/kotlin/cn/bobasyu/test/bangumi/BangumiTest.kt
+++ src/test/kotlin/cn/bobasyu/test/bangumi/BangumiTest.kt
@@ -3,11 +3,17 @@ package cn.bobasyu.test.bangumi;
 import cn.bobasyu.bangumi.BangumiRepository
 import cn.bobasyu.base.ApplicationContext
 import cn.bobasyu.entity.BangumiSearchDto
+import cn.bobasyu.entity.BangumiSubject
+import cn.bobasyu.entity.BangumiSubjectType
+import cn.bobasyu.utils.parseJson
+import cn.bobasyu.utils.toJson
 import io.vertx.core.Vertx
-import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtendWith
+import java.time.LocalDate
+import kotlin.test.assertEquals
 
 @ExtendWith(VertxExtension::class)
 class BangumiTest {
@@ -28,5 +34,28 @@ class BangumiTest {
         val resp = bangumiRepository.calendar()
         println(resp)
         testContext.completeNow()
+    }
+
+    @Test
+    fun jsonTest() {
+        val bangumiSubject =BangumiSubject(
+            id=0,
+            type = BangumiSubjectType.ANIMATION,
+            date = LocalDate.now(),
+        )
+        print(bangumiSubject.toJson())
+    }
+
+    @Test
+    fun jsonTest2() {
+        val bangumiSubject =BangumiSubject(
+            id=0,
+            type = BangumiSubjectType.ANIMATION,
+            date = LocalDate.now(),
+        )
+        val json = """
+            {"id":0,"type":2,"name":"","name_cn":"","summary":"","date":"2024-12-05","platform":"","images":{"large":"","common":"","medium":"","small":"","grid":""},"infoBox":"","volumes":null,"total_episodes":null}
+        """.trimIndent()
+        assertEquals(bangumiSubject, json.parseJson(BangumiSubject::class.java))
     }
 }
